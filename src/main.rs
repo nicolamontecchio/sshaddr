91765c4880ef2896f77934af3b10be5d608d6d2c
nicola montecchio <nicola.montecchio@gmail.com>
2016-09-23 22:22:33 +0200

    working (with default host)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use std::env;
 use std::path;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+
+extern crate regex;
+
+use regex::Regex;
 
 
 fn get_ssh_config_path() -> Option<path::PathBuf> {
@@ -18,9 +25,38 @@ fn get_ssh_config_path() -> Option<path::PathBuf> {
   )
 }
 
+fn get_hostname(fpath : path::PathBuf, host : String) -> Option<String> {
 
+  let host_re = Regex::new(r"^(?i)host\W+(.*)").unwrap();
+  let hostname_re = Regex::new(r"^(?i)hostname\W+(.*)").unwrap();
+
+  let p : &path::Path = fpath.as_path();
+  match File::open(p) {
+    Ok(f) => {
+      let mut host_found = false;
+      let lines = BufReader::new(&f);
+      for line in lines.lines() {
+        let l = line.unwrap();
+        if host_re.is_match(&l) {
+          let line_host = host_re.captures(&l).unwrap().at(1).unwrap();
+          if line_host == host.as_str() {
+            host_found = true;
+          }
+        } else if hostname_re.is_match(&l) && host_found {
+          let line_hostname = hostname_re.captures(&l).unwrap().at(1).unwrap();
+          // println!("{}", line_hostname);
+          return Some(line_hostname.to_string())
+        }
+      }
+      None
+    },
+    Err(_) => (None)
+  }
+
+}
 
 fn main() {
   get_ssh_config_path()
-    .map( |path| println!("path to ssh config is: {}", path.as_path().to_str().unwrap()) );
+    .and_then( |p| get_hostname(p, "tocai".to_string()))
+    .map_or( (), |h| println!("{}", h) )
 }
