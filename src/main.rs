805ef4d709a5608ec04bb5e5205f7f31c7125f16
nicola montecchio <nicola.montecchio@gmail.com>
2017-10-09 19:54:15 +0200

    list all hostnames if no arg given

--- src/main.rs
+++ src/main.rs
@@ -26,12 +26,9 @@ fn get_ssh_config_path() -> Option<path::PathBuf> {
 }
 
 fn get_hostname(fpath : path::PathBuf, host : String) -> Option<String> {
-
   let host_re = Regex::new(r"^(?i)host\W+(.*)").unwrap();
   let hostname_re = Regex::new(r"^(?i)hostname\W+(.*)").unwrap();
-
   let p : &path::Path = fpath.as_path();
-
   File::open(p)
     .ok()
     .and_then(|f|  {
@@ -51,13 +48,48 @@ fn get_hostname(fpath : path::PathBuf, host : String) -> Option<String> {
       }
       return None
     })
-
 }
 
+
+fn list_hostnames(fpath : path::PathBuf) -> Option<Vec<String>> {
+  let host_re = Regex::new(r"^(?i)host\W+(.*)").unwrap();
+  let p : &path::Path = fpath.as_path();
+  File::open(p)
+    .ok()
+    .and_then(|f|  {
+      let mut hosts : Vec<String> = Vec::new();
+      let lines = BufReader::new(&f);
+      for line in lines.lines() {
+        let l = line.unwrap();
+        if host_re.is_match(&l) {
+          let line_host = host_re.captures(&l).unwrap().at(1).unwrap().trim();
+          if line_host.len() > 0 {
+            hosts.push(line_host.to_string());
+          }
+        }
+      }
+      Some(hosts)
+    })
+}
+
+
 fn main() {
-  for arg in env::args() {
-    get_ssh_config_path()
-      .and_then( |p| get_hostname(p, arg.to_string()))
-      .map_or( (), |h| println!("{}", h) )
+  let args : Vec<String> = env::args().skip(1).collect();
+  match args.len() {
+    0 => {
+      get_ssh_config_path()
+        .and_then( |p| { list_hostnames(p)} )
+        .map_or( (), |h| {
+          for n in h {
+            println!("{}", n) }
+        })
+    },
+    _ => {
+      for arg in args {
+        get_ssh_config_path()
+          .and_then( |p| get_hostname(p, arg.to_string()))
+          .map_or( (), |h| println!("{}", h) )
+      }
+    }
   }
 }
